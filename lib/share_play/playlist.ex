a981cac53b7d39e693ea726648a58ee785a2417d
Leon Cruz <leon.cruz.teixeira@gmail.com>
2022-10-10 02:12:35 -0300

    feat: move database actions to another module

---
+++ lib/share_play/playlist.ex
@@ -0,0 +1,43 @@
+defmodule SharePlay.Playlist do
+  alias SharePlay.Repo
+  alias SharePlay.Playlist.Video
+
+  import Ecto.Query
+
+  def list_videos do
+    query = from(v in Video, order_by: [desc: v.updated_at])
+
+    Repo.all(query)
+  end
+
+  def video_changeset(video, params \\ %{}) do
+    Video.changeset(video, params)
+  end
+
+  def create_video(changeset) do
+    case Repo.insert(changeset) do
+      {:ok, _} -> :ok
+      {:error, %Ecto.Changeset{} = changeset} -> {:error, changeset}
+    end
+  end
+
+  def get_video(id) do
+    Repo.get(Video, id)
+  end
+
+  def update_video(changeset) do
+    case Repo.update(changeset) do
+      {:ok, _} -> :ok
+      {:error, %Ecto.Changeset{} = changeset} -> {:error, changeset}
+    end
+  end
+
+  def delete_video(id) do
+    video = get_video(id)
+
+    case Repo.delete(video) do
+      {:ok, _} -> :ok
+      {:error, _} -> :error
+    end
+  end
+end
--- lib/share_play_web/controllers/video_controller.ex
+++ lib/share_play_web/controllers/video_controller.ex
@@ -1,30 +1,26 @@
 defmodule SharePlayWeb.VideoController do
   use SharePlayWeb, :controller
 
-  alias SharePlay.Repo
+  alias SharePlay.Playlist
   alias SharePlay.Playlist.Video
 
-  import Ecto.Query
-
   def index(conn, _params) do
-    query = from(v in Video, order_by: [desc: v.updated_at])
-
-    videos = Repo.all(query)
+    videos = Playlist.list_videos()
 
     render(conn, "index.html", videos: videos)
   end
 
   def new(conn, _params) do
-    video = Video.changeset(%Video{})
+    video = Playlist.video_changeset(%Video{})
 
     render(conn, "new.html", video: video)
   end
 
   def create(conn, %{"video" => video_params}) do
-    changeset = Video.changeset(%Video{}, video_params)
+    changeset = Playlist.video_changeset(%Video{}, video_params)
 
-    case Repo.insert(changeset) do
-      {:ok, _} ->
+    case Playlist.create_video(changeset) do
+      :ok ->
         conn
         |> put_flash(:info, "Video created successfully.")
         |> redirect(to: Routes.video_path(conn, :index))
@@ -37,20 +33,20 @@ defmodule SharePlayWeb.VideoController do
   end
 
   def edit(conn, %{"id" => video_id}) do
-    video = Repo.get(Video, video_id)
+    video = Playlist.get_video(video_id)
 
-    video_changeset = Video.changeset(video)
+    video_changeset = Playlist.video_changeset(video)
 
     render(conn, "edit.html", video: video, video_changeset: video_changeset)
   end
 
   def update(conn, %{"id" => id, "video" => video_params}) do
-    video = Repo.get(Video, id)
+    video = Playlist.get_video(id)
 
-    changeset = Video.changeset(video, video_params)
+    changeset = Playlist.video_changeset(video, video_params)
 
-    case Repo.update(changeset) do
-      {:ok, _} ->
+    case Playlist.update_video(changeset) do
+      :ok ->
         conn
         |> put_flash(:info, "Video updated successfully.")
         |> redirect(to: Routes.video_path(conn, :index))
@@ -63,15 +59,13 @@ defmodule SharePlayWeb.VideoController do
   end
 
   def delete(conn, %{"id" => id}) do
-    video = Repo.get(Video, id)
-
-    case Repo.delete(video) do
-      {:ok, _} ->
+    case Playlist.delete_video(id) do
+      :ok ->
         conn
         |> put_flash(:info, "Video deleted successfully.")
         |> redirect(to: Routes.video_path(conn, :index))
 
-      {:error, _} ->
+      :error ->
         conn
         |> put_flash(:alert, "Video cannot be delted successfully.")
         |> render("index.html", conn: conn)
