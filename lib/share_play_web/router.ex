d125dc33d098beb164390cfdec52e7c42c2da02b
Leon Cruz <leon.cruz.teixeira@gmail.com>
2022-10-10 00:15:39 -0300

    feat: add routes

--- lib/share_play_web/router.ex
+++ lib/share_play_web/router.ex
@@ -18,6 +18,7 @@ defmodule SharePlayWeb.Router do
     pipe_through :browser
 
     get "/", PageController, :index
+    resources "/videos", VideoController, only: [:index, :new, :create]
   end
 
   # Other scopes may use custom stacks.
