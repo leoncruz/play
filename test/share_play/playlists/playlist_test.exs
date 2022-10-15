e3bfebefe975be32b6725443a039f522b105aed9
Leon Cruz <leon.cruz.teixeira@gmail.com>
2022-10-15 15:46:12 -0300

    add test to playlist

---
+++ test/share_play/playlists/playlist_test.exs
@@ -0,0 +1,15 @@
+defmodule SharePlay.PlaylistTest do
+  use SharePlay.DataCase, async: true
+
+  alias SharePlay.Playlists.Playlist
+
+  describe "changeet" do
+    test "name, category and video must be required" do
+      changeset = Playlist.changeset(%Playlist{}, %{name: "", category: "", videos: []})
+
+      assert %{name: ["can't be blank"]} = errors_on(changeset)
+      assert %{category: ["can't be blank"]} = errors_on(changeset)
+      assert %{videos: ["can't be blank"]} = errors_on(changeset)
+    end
+  end
+end
