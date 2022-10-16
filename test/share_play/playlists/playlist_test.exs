a4f3fe17cf3608be9a86233de3dbff280df8198b
Leon Cruz <leon.cruz.teixeira@gmail.com>
2022-10-15 21:28:17 -0300

    refactor: impromve tests

--- test/share_play/playlists/playlist_test.exs
+++ test/share_play/playlists/playlist_test.exs
@@ -1,14 +1,52 @@
-defmodule SharePlay.PlaylistTest do
+defmodule SharePlay.Playlists.PlaylistTest do
   use SharePlay.DataCase, async: true
 
+  alias Ecto.Changeset
   alias SharePlay.Playlists.Playlist
 
   describe "changeset/1" do
-    test "name, category and video must be required" do
-      changeset = Playlist.changeset(%Playlist{}, %{name: "", category: "", videos: []})
+    setup do
+      video = %{name: "video #1", url: "http://example.com"}
 
+      params = %{
+        name: "playlist #1",
+        category: "category #1",
+        videos: [video]
+      }
+
+      %{params: params}
+    end
+
+    test "sucess: return valid changeset when given valid arguments", %{params: params} do
+      changeset = Playlist.changeset(%Playlist{}, params)
+
+      assert %Changeset{valid?: true, changes: _} = changeset
+    end
+
+    test "failure: return invalid changeset when name is not given", %{params: params} do
+      params = Map.delete(params, :name)
+
+      changeset = Playlist.changeset(%Playlist{}, params)
+
+      assert %Changeset{valid?: false, changes: _} = changeset
       assert %{name: ["can't be blank"]} = errors_on(changeset)
+    end
+
+    test "failure: return invalid changeset when category is not given", %{params: params} do
+      params = Map.delete(params, :category)
+
+      changeset = Playlist.changeset(%Playlist{}, params)
+
+      assert %Changeset{valid?: false, changes: _} = changeset
       assert %{category: ["can't be blank"]} = errors_on(changeset)
+    end
+
+    test "failure: return invalid changeset when videos are not given", %{params: params} do
+      params = Map.delete(params, :videos)
+
+      changeset = Playlist.changeset(%Playlist{}, params)
+
+      assert %Changeset{valid?: false, changes: _} = changeset
       assert %{videos: ["can't be blank"]} = errors_on(changeset)
     end
   end
--- test/share_play/playlists/video_test.exs
+++ test/share_play/playlists/video_test.exs
@@ -1,26 +1,66 @@
-defmodule SharePlay.VideoTest do
+defmodule SharePlay.Playlists.VideoTest do
   use SharePlay.DataCase, async: true
 
   alias SharePlay.Playlists.Video
+  alias Ecto.Changeset
 
-  describe "changeset" do
-    test "name, url and playlist_id must be required" do
-      changeset = Video.changeset(%Video{}, %{name: "", url: "", playlist_id: ""})
+  describe "changeset/1" do
+    setup do
+      %{params: %{name: "video #1", url: "http://example.com", playlist_id: 1}}
+    end
 
-      assert %{url: ["can't be blank"]} = errors_on(changeset)
+    test "sucess: return valid changeset when given valid arguments", %{params: params} do
+      changeset = Video.changeset(%Video{}, params)
+
+      assert %Changeset{valid?: true, changes: _} = changeset
+    end
+
+    test "failure: return invalid changeset when name is not given", %{params: params} do
+      params = Map.delete(params, :name)
+
+      changeset = Video.changeset(%Video{}, params)
+
+      assert %Changeset{valid?: false, changes: _} = changeset
       assert %{name: ["can't be blank"]} = errors_on(changeset)
-      assert %{playlist_id: ["can't be blank"]} = errors_on(changeset)
+    end
+
+    test "failure: return invalid changeset when url is not given", %{params: params} do
+      params = Map.delete(params, :url)
+
+      changeset = Video.changeset(%Video{}, params)
+
+      assert %Changeset{valid?: false, changes: _} = changeset
+      assert %{url: ["can't be blank"]} = errors_on(changeset)
     end
   end
 
-  describe "playlist_video_changeset" do
-    test "name ad url must be required" do
-      changeset = Video.playlist_video_changeset(%Video{}, %{name: "", url: "", playlist_id: ""})
+  describe "playlist_video_changeset/1" do
+    setup do
+      %{params: %{name: "video #1", url: "http://example.com"}}
+    end
 
-      assert %{url: ["can't be blank"]} = errors_on(changeset)
+    test "sucess: return valid changeset when given valid arguments", %{params: params} do
+      changeset = Video.playlist_video_changeset(%Video{}, params)
+
+      assert %Changeset{valid?: true, changes: _} = changeset
+    end
+
+    test "failure: return invalid changeset when name is not given", %{params: params} do
+      params = Map.delete(params, :name)
+
+      changeset = Video.changeset(%Video{}, params)
+
+      assert %Changeset{valid?: false, changes: _} = changeset
       assert %{name: ["can't be blank"]} = errors_on(changeset)
+    end
 
-      refute match?(%{playlist_id: ["can't be blank"]}, errors_on(changeset))
+    test "failure: return invalid changeset when url is not given", %{params: params} do
+      params = Map.delete(params, :url)
+
+      changeset = Video.changeset(%Video{}, params)
+
+      assert %Changeset{valid?: false, changes: _} = changeset
+      assert %{url: ["can't be blank"]} = errors_on(changeset)
     end
   end
 end
